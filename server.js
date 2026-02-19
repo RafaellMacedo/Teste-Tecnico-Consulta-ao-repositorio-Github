21deef39fa04ceb95aa468013bdc9ec45757053b
Rafael Luiz de Macedo <rafael.macedo-montreal@pbh.gov.br>
2026-02-19 15:28:48 -0300

    Criado rota para consultar os repositórios do nome do usuário passado na request

--- server.js
+++ server.js
@@ -11,4 +11,37 @@ app.get("/select", async (req, res) => {
     res.json(result.rows);
 });
 
+app.get("/repositories", async (req, res) => {
+    const response = await fetch('https://api.github.com/users/' + req.query.username + '/repos');
+
+    if (!response.ok) {
+        return res.status(response.status).json({
+            error: "Erro ao acessar API externa"
+        });
+    }
+
+    const data = await response.json();
+
+    if (!data || (Array.isArray(data) && data.length === 0)) {
+        return res.json({ message: "Nenhum dado encontrado" });
+    }
+
+    const todosRepositorios = [];
+
+    data.forEach((repo, index) => {
+        if (index < 20) {
+            todosRepositorios.push({
+                name: repo.name,
+                url: repo.html_url,
+                description: repo.description,
+                language: repo.language,
+                start: repo.stargazers_count,
+                last_update: repo.updated_at
+            });
+        }
+    });
+
+    res.json(todosRepositorios);
+});
+
 app.listen(5000, () => console.log("Server running on port 5000"));
